91be440d9964a392c7e7827fa63e86af92385edc
Takayuki Ohtake <takayuki.ohtake@gaiax.com>
2020-06-01 15:49:47 +0900

    Login with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root 'root#index'
   post '/create_session', controller: :root, action: :create_session
   get '/success', controller: :root, action: :success
